322cb29c637e464856b868082ad38a070176fba9
Kevin Thompson <kevin.thompson@verizon.com>
2014-12-31 10:10:45 -0600

    adds a random list of vendors that participated in creating the report

--- server.rb
+++ server.rb
@@ -15,8 +15,29 @@ def getActor(inTable)
   end
 end
 
+def to_sentence(inArray)
+  case inArray.length
+    when 0
+      return ""
+    when 1
+      return "#{inArray[0]}"
+    when 2
+      return "#{inArray[0]} and #{inArray[1]}"
+    else
+      "#{inArray[0...-1].join(', ')}, and #{inArray[-1]}"
+  end
+end
+
+SECURITY_VENDORS = ["Verizon", "Norse", "Crowdstrike",
+  "iDefense", "Mandiant/FireEye", "Dell SecureWorks",
+  "McAfee", "Symantec", "Kaspersky Labs", "Palo Alto Networks",
+  "Fortinet", "iSIGHT Partners", "Check Point", "Arbor Networks",
+  "Qualys", "Trend Micro", "Veracode"]
+
 get '/' do
   @actor = getActor(freqs)
   @how_many_researchers = rand(2..10)
+  @vendors = to_sentence(SECURITY_VENDORS.sample(rand(1..@how_many_researchers)))
+
   erb :index
 end
